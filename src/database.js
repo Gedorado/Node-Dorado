467e3c6311c56900acdcd170a3cc4cf3a522f861
Gedorado <85143189+Gedorado@users.noreply.github.com>
2021-09-18 12:56:27 -0500

    update

--- src/database.js
+++ src/database.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
+import {createRoles, createAdmin}  from './libs/initialSetup'
 
-
-mongoose.connect("mongodb://localhost/companydb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((db) => console.log('DB is connected'))
-  .catch((err) => console.log(err));
+mongoose.connect("mongodb+srv://UPS:UPS2020@cluster0.cyt4q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").then(db=>{
+    console.log('Db is connected');
+    createRoles();
+    createAdmin();
+}).catch(error=> console.log(error))
