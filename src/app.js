0c738ed7d685ef3b925c050a13c0a36d1338867d
Gedorado <85143189+Gedorado@users.noreply.github.com>
2021-09-18 12:55:49 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
-import express from "express"
-import morgan from "morgan";
-import pkg from '../package.json'
-import productsRoutes from './routes/products.routes'
-const app=express()
+import express from 'express'
+import morgan from 'morgan'
+import pkg from "../package.json"
 
-app.set('pkg',pkg);
+import productsRoutes from './routes/products.routes' 
+import authRoutes from './routes/auth.routes'
+import userRoutes from './routes/user.routes'
+
+const app = express()
+app.set('pkg',pkg)
 app.use(morgan('dev'));
-app.get('/',(req, res) =>{
+app.use(express.json());
+
+app.get('/',(req,res)=>{
     res.json({
-        name: app.get('pkg').name,
-        author: app.get('pkg').author,
+        author:app.get('pkg').author,
+        name:app.get('pkg').name,
         description:app.get('pkg').description,
-        version: app.get('pkg').version
-    })
-})
-app.use(productsRoutes)
+        version:app.get('pkg').version
+    });
+});
+
+app.use('/api/products',productsRoutes)
+app.use('/api/auth',authRoutes)
+app.use('/api/users',userRoutes)
+
+
 export default app;
